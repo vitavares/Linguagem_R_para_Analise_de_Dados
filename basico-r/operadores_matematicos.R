b3439f308cd8bd42536f7cb59fdcf8a88e605862
Vitória Tavares <vitoriadesousat@gmail.com>
2023-05-11 16:51:39 -0300

    aula de operadores

---
+++ basico-r/operadores_matematicos.R
@@ -0,0 +1,71 @@
+#Soma
+5 + 3
+
+#Subtração
+8 - 6
+
+#Multiplicação
+4 * 3
+
+#Divisão
+20 / 5
+
+#Potenciação
+4 ** 2
+4 ^ 2
+
+## RADICIAÇÃO
+# Raiz quadrada
+sqrt(81)
+81**(1/2)
+#Raiz cúbica
+125**(1/3)
+27**(1/3)
+
+#Módulo
+abs(-10)
+abs(10)
+
+#Fatorial
+factorial(3)
+factorial(5)
+
+#Logaritmo (base 2 e base 10)
+log10(100) #10^2 = 100
+log2(8) # 2^3 = 8
+
+#Exponencial
+exp(1)
+exp(2)
+exp(-2)
+
+#Logaritmo natural
+log(2.718281828)
+log(100)
+
+#Constante pi
+pi
+
+#Pi radianos = 180graus
+
+#Funçoes trigonometricas (radianos)
+sin(pi/6) #pi/6 radianos é igual a 30 graus
+asin(0.5)
+
+cos(pi/4) #pi/4 radianos é igual a 45 graus
+acos(0.7071068)
+
+tan(pi/4) #pi/4 radianos é igual a 45 graus
+atan(1)
+
+#Notação cientifica
+2e32 #(2*10^32)
+2e3
+2e-26
+2e5 + 4e5
+3e20 * 2e12
+15e16 / 5e6
+
+#Verificar versão do r
+getRversion()
+
